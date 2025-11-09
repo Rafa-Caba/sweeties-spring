2af015a78de755634ffa85a3274a6b75668acdf8
Rafael Cabanillas <rafaelcabanillas12@gmail.com>
2025-11-09 15:51:25 -0600

    fix(db): Add missing fields to items

--- src/main/resources/db/migration/V12__create_item_sprites_table.sql
+++ src/main/resources/db/migration/V12__create_item_sprites_table.sql
@@ -1,5 +1,5 @@
---CREATE TABLE item_sprites (
---    item_id BIGINT NOT NULL,
---    sprite_url VARCHAR(512),
---    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
---);
+CREATE TABLE item_sprites (
+    item_id BIGINT NOT NULL,
+    sprite_url VARCHAR(512),
+    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+);
